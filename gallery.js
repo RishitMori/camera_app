65a6f121ea795ac5146c8e0867fcf9a43e24c5fb
RishitMori <rishit1640@gmail.com>
2026-08-11 17:26:47 +0530

    [UPDATED] part 2

--- gallery.js
+++ gallery.js
@@ -13,7 +13,7 @@ setTimeout(() => {
         mediaElem.setAttribute("id", videoObj.id);
 
         let url = URL.createObjectURL(videoObj.blobData);
-        1wqewre
+        1wqewreedf
         mediaElem.innerHTML = `
                 <div class="media">
                     <video autoplay loop muted src="${url}"></video>
